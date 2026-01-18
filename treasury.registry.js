eb8e156fb84cee840294495e09bfa7443397b7a6
Akash <161524961+akash8987@users.noreply.github.com>
2026-01-18 15:01:22 +0600

    Create treasury.registry.js

---
+++ treasury.registry.js
@@ -0,0 +1,9 @@
+export function loadTreasury() {
+  console.log("Loading treasury...");
+
+  return {
+    name: "DAO Treasury",
+    balance: 10000,
+    categories: ["Development", "Marketing", "Operations"]
+  };
+}
